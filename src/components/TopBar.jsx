14425d9371463c78cc499da0cfb149fa6a61fdf9
Eduard <eduard.aminov.160@gmail.com>
2020-06-07 22:38:00 +0300

    Add TopBar.jsx

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+
+const TopBar = () => {
+    return (
+        <nav className="navbar navbar-light">
+            <div className="container">
+                <Link className='navbar-brand' to={'/'}>
+                    Home
+                </Link>
+                <ul className="nav navbar-nav pull-xs-right">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to='/' exact>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to='/login' exact>
+                            Sign-in
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to='/register' exact>
+                            Sign-up
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default TopBar
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
+import TopBar from './components/TopBar'
 import Routes from './routes'
 
 const App = () => {
     return (
         <Router>
-            <Routes />
+            <TopBar/>
+            <Routes/>
         </Router>
     )
 }

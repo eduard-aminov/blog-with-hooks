3b548686c40d72c33cedb13500cfe71ea0e3cca7
Eduard <eduard.aminov.160@gmail.com>
2020-06-07 22:55:23 +0300

    Add Auth.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Article from './pages/article/Article'
+import Auth from './pages/auth/Auth'
 import GlobalFeed from './pages/globalFeed/GlobalFeed'
 
 const Routes = () => {
     return (
         <Switch>
             <Route path={'/'} component={GlobalFeed} exact />
+            <Route path={'/login'} component={Auth} />
+            <Route path={'/register'} component={Auth} />
             <Route path={'/article/:slug'} component={Article} />
         </Switch>
     )

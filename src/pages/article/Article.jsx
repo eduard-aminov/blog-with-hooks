5cd9393722313389e512b7242ff0b4da005fc5d5
Eduard <eduard.aminov.160@gmail.com>
2020-06-12 13:04:07 +0300

    Add article deletion functionality

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -1,49 +1,103 @@
-import React, {useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import React, {useContext, useEffect, useState} from 'react'
+import {Link, Redirect} from 'react-router-dom'
 import ErrorMessage from '../../components/ErrorMessage'
 import Loading from '../../components/Loading'
 import TagList from '../../components/TagList'
+import {CurrentUserContext} from '../../contexts/currentUserContext'
 import useFetch from '../../hooks/useFetch'
 
 const Article = ({match}) => {
     const slug = match.params.slug
     const apiUrl = `/articles/${slug}`
-    const [{response, isLoading, error}, doFetch] = useFetch(apiUrl)
+    const [{
+        response: fetchArticleResponse,
+        isLoading: fetchArticleIsLoading,
+        error: fetchArticleError},
+        doFetchArticle
+    ] = useFetch(apiUrl)
+    const [{response: deleteArticleResponse}, doDeleteArticle] = useFetch(apiUrl)
+    const [currentUserState] = useContext(CurrentUserContext)
+    const [successfullDelete, setSuccessfulDelete] = useState(false)
+
+    const isAuthor = () => {
+        if (!fetchArticleResponse || !currentUserState.isLoggedIn) {
+            return false
+        }
+        return currentUserState.currentUser.username === fetchArticleResponse.article.author.username
+    }
 
     useEffect(() => {
-        doFetch()
-    },[doFetch])
+        doFetchArticle()
+    },[doFetchArticle])
+
+    useEffect(() => {
+        if (!deleteArticleResponse) {
+            return
+        }
+        setSuccessfulDelete(true)
+    }, [deleteArticleResponse])
+
+    const deleteArticle = () => {
+        doDeleteArticle({
+            method: 'delete'
+        })
+    }
+
+    if (successfullDelete) {
+        return <Redirect to={'/'}/>
+    }
 
     return (
         <div className="article-page">
             <div className="banner">
-                {!isLoading && response && (
+                {!fetchArticleIsLoading && fetchArticleResponse && (
                     <div className="container">
-                        <h1>{response.article.title}</h1>
+                        <h1>{fetchArticleResponse.article.title}</h1>
                         <div className="article-meta">
-                            <Link to={`/profiles/${response.article.author.username}`}>
-                                <img src={response.article.author.image} alt=""/>
+                            <Link to={`/profiles/${fetchArticleResponse.article.author.username}`}>
+                                <img src={fetchArticleResponse.article.author.image} alt=""/>
                             </Link>
                             <div className="info">
-                                <Link to={`/profiles/${response.article.author.username}`}>
-                                    {response.article.author.username}
+                                <Link to={`/profiles/${fetchArticleResponse.article.author.username}`}>
+                                    {fetchArticleResponse.article.author.username}
                                 </Link>
-                                <span className="date">{response.article.createdAt}</span>
+                                <span className="date">{fetchArticleResponse.article.createdAt}</span>
+                                <div>
+                                    {isAuthor() && (
+                                        <span>
+                                            <Link
+                                                className='btn btn-outline-secondary btn-sm'
+                                                to={`/article/${fetchArticleResponse.article.slug}/edit`}
+                                            >
+                                                <i className='ion-edit'></i>
+                                                &nbsp; Edit Article
+                                            </Link>
+                                            &nbsp;
+                                            <button
+                                                className='btn btn-outline-danger btn-sm'
+                                                onClick={deleteArticle}
+                                            >
+                                                <i className='ion-trash-a'></i>
+                                                &nbsp; Delete Article
+                                            </button>
+                                        </span>
+                                    )}
+                                </div>
                             </div>
                         </div>
                     </div>
                 )}
             </div>
             <div className="container page">
-                {isLoading && <Loading/>}
-                {error && <ErrorMessage/>}
-                {!isLoading && response && (
+                {fetchArticleIsLoading && <Loading/>}
+                {fetchArticleError && <ErrorMessage/>}
+                {!fetchArticleIsLoading && fetchArticleResponse && (
                     <div className="row article-content">
                         <div className="col-xs-12">
                             <div>
-                                <p>{response.article.body}</p>
+                                <p>{fetchArticleResponse.article.body}</p>
                             </div>
-                            <TagList tags={response.article.tagList} />
+                            <TagList tags={fetchArticleResponse.article.tagList} />
                         </div>
                     </div>
                 )}
